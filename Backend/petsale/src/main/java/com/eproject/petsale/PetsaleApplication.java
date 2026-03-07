cea7244a1c2ef998ff2d471eeded29d2b4d6c0cd
datlaipro <datnuocep@gmail.com>
2026-03-07 13:23:25 +0700

    khởi tại be

---
+++ Backend/petsale/src/main/java/com/eproject/petsale/PetsaleApplication.java
@@ -0,0 +1,13 @@
+package com.eproject.petsale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsaleApplication.class, args);
+	}
+
+}
---
+++ Backend/petsale/src/test/java/com/eproject/petsale/PetsaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eproject.petsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
