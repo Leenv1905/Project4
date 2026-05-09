1beca51aff8d487aeefd3a2a69e333426a808058
Lee Jung Zee <lee.nv1905@gmail.com>
2026-05-10 06:49:37 +0700

    chỉnh sửa css

--- Frontend/pet/src/app/shared/home-components/for-features/categories-section/categories-section.component.ts
+++ Frontend/pet/src/app/shared/home-components/for-features/categories-section/categories-section.component.ts
@@ -7,14 +7,28 @@ import { RouterModule } from '@angular/router';
   selector: 'app-categories-section',
   imports: [CommonModule, RouterModule],
   templateUrl: './categories-section.component.html',
-  styleUrls: ['./categories-section.component.scss']
+  styleUrls: ['./categories-section.component.scss'],
 })
 export class CategoriesSectionComponent {
-
   categories = [
-    { title: 'Poodle', image: '/assets/cho21.jpg', link: '/shop' },
-    { title: 'Poodle', image: '/assets/cho21.jpg', link: '/shop' },
-    { title: 'Alaska', image: '/assets/cho21.jpg', link: '/shop' },
-    { title: 'Beagle', image: '/assets/cho21.jpg', link: '/shop' }
+    {
+      title: 'Poodle',
+      image:
+        'https://cdn.tgdd.vn/Files/2021/04/12/1342796/15-giong-cho-canh-dep-de-cham-soc-pho-bien-tai-viet-nam-202104121459561436.jpg',
+      link: '/shop',
+    },
+    {
+      title: 'Corgi',
+      image:
+        'https://cdn.tgdd.vn/Files/2021/04/12/1342796/15-giong-cho-canh-dep-de-cham-soc-pho-bien-tai-viet-nam-202104121504407738.jpg',
+      link: '/shop',
+    },
+    {
+      title: 'Alaska',
+      image:
+        'https://cdn.tgdd.vn/Files/2021/04/12/1342796/15-giong-cho-canh-dep-de-cham-soc-pho-bien-tai-viet-nam-202104121501444654.jpg',
+      link: '/shop',
+    },
+    { title: 'Chihuahua', image: '/assets/cho21.jpg', link: '/shop' },
   ];
 }
